ed72f2eb24546ef5f1ca22a24ef9a4c6a5e81d72
Damien Le Berrigaud <damien@itsdamo.com>
2017-01-04 14:53:29 -0700

    databse background job checkout

--- src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
@@ -51,7 +51,7 @@ public class MoviesBean {
     }
 
     @Transactional
-    public void editMovie(Movie movie) {
+    public void updateMovie(Movie movie) {
         entityManager.merge(movie);
     }
 
