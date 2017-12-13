ff7c147b03855318b43777cd6617ed881bb05371
Thakur <anju.thakur@accenture.com>
2017-12-13 10:37:45 -0600

    adding the final working copy for background jobs

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -3,9 +3,12 @@ package org.superbiz.moviefun.albums;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAsync
@@ -14,12 +17,14 @@ public class AlbumsUpdateScheduler {
 
     private static final long SECONDS = 1000;
     private static final long MINUTES = 60 * SECONDS;
+    private final DataSource objDataSource;
 
     private final AlbumsUpdater albumsUpdater;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater) {
+    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater,DataSource objDataSource) {
         this.albumsUpdater = albumsUpdater;
+        this .objDataSource=objDataSource;
     }
 
 
@@ -27,12 +32,39 @@ public class AlbumsUpdateScheduler {
     public void run() {
         try {
             logger.debug("Starting albums update");
-            albumsUpdater.update();
+
+            if (startAlbumSchedulerTask()) {
+                logger.debug("Starting albums update");
+                albumsUpdater.update();
+                logger.debug("Finished albums update");
+
+            } else {
+                logger.debug("Nothing to start");
+            }
 
             logger.debug("Finished albums update");
 
         } catch (Throwable e) {
             logger.error("Error while updating albums", e);
         }
+    }
+
+    private boolean startAlbumSchedulerTask()
+    {
+        boolean status = false;
+        JdbcTemplate jdbcTemp = new JdbcTemplate(objDataSource);
+        int updatedRows = jdbcTemp.update(
+                "UPDATE album_scheduler_task" +
+                        " SET started_at = now()" +
+                        " WHERE started_at IS NULL" +
+                        " OR started_at < date_sub(now(), INTERVAL 2 MINUTE)"
+        );
+
+        if(updatedRows > 0)
+        {
+            status = true;
+        }
+        return status;
+
     }
 }
